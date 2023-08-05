e83d73ee4644156829b71d0caf6d9a1a3c7f732b
rjangid9080 <grut.rjangid9080@gmail.com>
2023-08-05 23:23:55 +0530

    email-verification page

--- src/app/verify-email/page.js
+++ src/app/verify-email/page.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useState, useLayoutEffect } from "react";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
+import Image from "next/image";
 
 const VerifyEmail = () => {
   useLayoutEffect(() => {
@@ -35,8 +36,21 @@ const VerifyEmail = () => {
       <Toaster position="top-center" reverseOrder={true} />
       <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
         <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-          <h1 className="mb-8 text-3xl text-center">Email Verified</h1>
-          <p>Your Email has been verified.You can login now.</p>
+          <div className="w-full border-b flex justify-center p-2">
+            <Image
+              src="mail.svg"
+              alt="Email Verified"
+              width={150}
+              height={100}
+            />
+          </div>
+          <h1 className="mb-8 mt-8 text-3xl text-center">Email Verified</h1>
+          <Link
+            className="w-full flex justify-center justify-self-center items-center text-center py-3 rounded bg-blue-500 text-white hover:bg-blue-600 focus:outline-none my-1"
+            href="/login"
+          >
+            Login Now
+          </Link>
         </div>
       </div>
     </div>
